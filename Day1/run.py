27e85aed4516f66ea95b105a9085a7b232ec9142
Martijn <oldmartijntje@gmail.com>
2023-12-06 09:34:49 +0100

    day 1 part 2 completed

--- Day1/run.py
+++ Day1/run.py
@@ -2,6 +2,18 @@ numbers = []
 first = -1
 last = -1
 total = 0
+rename = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
 
 f = open("input.txt", "r")
 data = f.read().splitlines()
@@ -10,12 +22,21 @@ print(data)
 for line in data:
     first = -1
     last = -1
-    for char in line:
-       if char.isdigit():
-           if first == -1:
-               first = int(char)
-           else:
-               last = int(char)
+    for x in range(len(line)):
+        if line[0].isdigit():
+            if first == -1:
+               first = int(line[0])
+            else:
+               last = int(line[0])
+        else:
+            for key in rename:
+                if line.startswith(key):
+                    if first == -1:
+                        first = rename[key]
+                    else:
+                        last = rename[key]
+        line = line[1:]
+        
     if last == -1:
         last = first
     numbers.append(f"{first}{last}")
